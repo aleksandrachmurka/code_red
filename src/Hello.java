f81d1a8c8a686fd8b7b733fcee839ad752033c45
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-04-08 20:02:20 +0200

    Add new class

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+
+    public static void main(String... args) {
+        String name = "Ola";
+
+        System.out.println("Hello, my name is " + name);
+        System.out.println("Bye World!");
+    }
+}
