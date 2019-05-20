640088ddf7f4db32b7f98ac5e821c12726abd515
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-05-20 19:48:56 +0200

    Add methods exercises

---
+++ src/CalculateAverage.java
@@ -0,0 +1,15 @@
+//public class CalculateAverage {
+//
+//    public static int average(int[] args) {
+//        for (int i = 0, total = 1; i < args.length(); i++) {
+//            total *= args[i]
+//        }
+//
+//        return total/args.length;
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(average([5, 6]));
+//    }
+//}
